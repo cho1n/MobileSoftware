69def36c18bff5de0a4a76b119967b33bf9b6b1f
cho1n <cjaewon0217@naver.com>
2023-11-12 22:20:18 +0900

    식당 설정 Spinner 추가

--- app/src/main/java/com/example/mobilesoftware/MainActivity.java
+++ app/src/main/java/com/example/mobilesoftware/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mobilesoftware;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner placeSpinner = (Spinner) findViewById(R.id.spinner_place);
+        ArrayAdapter placeAdapter = ArrayAdapter.createFromResource(this,
+                R.array.place_for_food, android.R.layout.simple_spinner_item);
+        placeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        placeSpinner.setAdapter(placeAdapter);
     }
 }
