f526975163339efff2b65e4ceb83e40804ea8af5
cho1n <cjaewon0217@naver.com>
2023-11-22 20:54:01 +0900

    Feat: 하단 메뉴바 추가

--- app/src/main/java/com/example/mobilesoftware/HomeActivity.java
+++ app/src/main/java/com/example/mobilesoftware/HomeActivity.java
@@ -1,15 +1,57 @@
 package com.example.mobilesoftware;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private MainMenuChart fragmentChart = new MainMenuChart();
+    private MainMenuMoreFragment fragmentMore = new MainMenuMoreFragment();
+    private MainMenuSearchFragment fragmentSearch = new MainMenuSearchFragment();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.menu_frame_layout, fragmentChart).commitAllowingStateLoss();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.menu_bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
+    }
+
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch (menuItem.getItemId()) {
+                case R.id.menu_input:
+                    transaction.replace(R.id.menu_frame_layout, fragmentChart).commitAllowingStateLoss();
+                    break;
+                case R.id.menu_search:
+                    transaction.replace(R.id.menu_frame_layout, fragmentSearch).commitAllowingStateLoss();
+                    break;
+                case R.id.menu_month:
+                    transaction.replace(R.id.menu_frame_layout, fragmentMore).commitAllowingStateLoss();
+                    break;
+                case R.id.menu_home:
+                    Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+            return true;
+        }
     }
 }
